82eac1205903961aee6ee512de8dd426a24f4ffc
Byron Silva Olvera <byronsjimb@gmail.com>
2021-05-20 16:35:58 -0500

    vista log in out

--- menucrud/Contenido.js
+++ menucrud/Contenido.js
@@ -2,10 +2,28 @@ const Route = ReactRouterDOM.Route;
 
 const Contenido = () =>(
     <div className="nav-lateral-der">
+        <div className="header-contenido" >
+            <div className="item-name">
+                Desarollo Web React CDN 
+            </div>
+            <div className="grid-item item-log-in-out">
+                <div>
+                    <span>Log-In</span>
+                    <span class="material-icons">login</span>
+                </div>
+                <div>
+                    <span>Log-out</span>
+                    <span class="material-icons">logout</span>
+                </div>
+            </div>
+        </div>
+
+        <Route path="/home" exact component={Add} />
         <Route path="/add" exact component={Add} />
         <Route path="/edit" exact component={Edit} />
         <Route path="/get" exact component={Get} />    
         <Route path="/delete" component={Delete} />    
         <Route path="/list" component={List} />    
+    
     </div>
 )
